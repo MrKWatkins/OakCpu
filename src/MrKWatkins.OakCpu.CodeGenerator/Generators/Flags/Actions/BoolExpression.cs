9f4af89d4f721c8c1f25fa93bd2ef31a56983917
Kevin Watkins <mrkwatkins@gmail.com>
2025-06-16 20:28:52 +0100

    Benchmark showing CMOVs are the way to go.

--- src/MrKWatkins.OakCpu.CodeGenerator/Generators/Flags/Actions/BoolExpression.cs
+++ src/MrKWatkins.OakCpu.CodeGenerator/Generators/Flags/Actions/BoolExpression.cs
@@ -25,6 +25,7 @@ internal sealed class BoolExpression : FlagAction
         var boolExpression = ExpressionGenerator.GenerateExpressionSyntax(context, Expression);
 
         // Tried using a ternary instead to get a CMOV to select the correct bitmask but the JIT would not play ball.
+        // TODO: Benchmarks show CMOVs are twice as fast. Try moving flags to separate functions to get CMOVs. Will certainly get shorter code. Could also use function parameters instead of field access.
         var byteExpression = GenerateBitCastFromBoolToByte(context, boolExpression);
 
         var shift = Flags[0].Index;
---
+++ src/MrKWatkins.OakCpu.Z80.Benchmarks/BitCastVersusCmovBenchmark.cs
@@ -0,0 +1,41 @@
+using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
+
+namespace MrKWatkins.OakCpu.Z80.Benchmarks;
+
+[DisassemblyDiagnoser]
+public class BitCastVersusCmovBenchmark
+{
+    private byte A;
+    private byte F;
+
+    [Benchmark(Baseline = true)]
+    public void BitCast()
+    {
+        A = (byte)(A + 0x01);
+        // Flags.
+        var flags = 0b00000000; // Reset N.
+        flags |= A & 0b00101000; // Copy X and Y from A.
+        flags |= F & 0b00000001; // Copy C from F.
+        flags |= Unsafe.BitCast<bool, byte>(A == 0x80) << 2; // Set PV if A == 0x80 is true.
+        flags |= Unsafe.BitCast<bool, byte>((A & 0x0F) == 0x00) << 4; // Set H if (A & 0x0F) == 0x00 is true.
+        flags |= Unsafe.BitCast<bool, byte>(A == 0) << 6; // Set Z if is_zero(A) is true.
+        flags |= A & 0x80; // Set S if is_negative(A) is true.
+        F = (byte)flags;
+    }
+
+    [Benchmark]
+    public void Cmov()
+    {
+        A = (byte)(A + 0x01);
+        // Flags.
+        var flags = 0b00000000; // Reset N.
+        flags |= A & 0b00101000; // Copy X and Y from A.
+        flags |= F & 0b00000001; // Copy C from F.
+        flags |= A == 0x80 ? 4 : 0; // Set PV if A == 0x80 is true.
+        flags |= (A & 0x0F) == 0x00 ? 16 : 0; // Set H if (A & 0x0F) == 0x00 is true.
+        flags |= A == 0 ? 64 : 0; // Set Z if is_zero(A) is true.
+        flags |= A & 0x80; // Set S if is_negative(A) is true.
+        F = (byte)flags;
+    }
+}
