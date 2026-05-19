2b3146937e9400508c5da9e33c554a93485a73d3
Kevin Watkins <mrkwatkins@gmail.com>
2026-05-19 20:56:42 +0100

    Removed the old disassemble project.

--- src/Disassemble.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export DOTNET_JitDisasm=Step
-export DOTNET_TieredCompilation=0
-
-dotnet run --project MrKWatkins.OakCpu.Z80.Disassemble --configuration Release > Disassembly.txt
--- src/MrKWatkins.OakCpu.Z80.Disassemble/Program.cs
+++
@@ -1,8 +0,0 @@
-using MrKWatkins.OakCpu.Z80;
-
-var emulator = new Z80StepEmulator();
-for (var f = 0; f < 5; f++)
-{
-    emulator.Step();
-    emulator.Data = 0x3C;
-}
